0389b8fe330b7b26a9f429d9ba758fb5213ec1e6
Nathan Miguel <nathan.cm77@gmail.com>
2024-03-28 22:33:25 -0300

    Fixed category name

--- Source/Websocket_MMO/Public/WebsocketFunctionLibrary.h
+++ Source/Websocket_MMO/Public/WebsocketFunctionLibrary.h
@@ -14,44 +14,44 @@ class WEBSOCKET_MMO_API UWebsocketFunctionLibrary : public UBlueprintFunctionLib
 {
 	GENERATED_BODY()
 public:
-	UFUNCTION(BlueprintCallable, Category = "Websocket||Binary")
+	UFUNCTION(BlueprintCallable, Category = "Websockets||Binary")
 	static bool GetMessageFromEvent(const FString& EventName, const FString& EventMessage, FString& Message);
 
-	UFUNCTION(BlueprintCallable, Category = "Websocket||Binary")
+	UFUNCTION(BlueprintCallable, Category = "Websockets||Binary")
 	static bool GetRawDataFromMessage(const FString& EventName, const FString& EventMessage, TArray<uint8>& Data);
 
 	// Convert bool to byte array | Convert byte array to bool	
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websocket||Binary")
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websockets||Binary")
 	static TArray<uint8> BoolToByteArray(const bool& value);
 
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websocket||Binary")
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websockets||Binary")
 	static bool ByteArrayToBoolean(const TArray<uint8>& ByteArray);
 	
 	// Convert int to byte array | Convert byte array to int
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websocket||Binary")
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websockets||Binary")
 	static TArray<uint8> IntToByteArray(const int& value);
 
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websocket||Binary")
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websockets||Binary")
 	static int ByteArrayToInt(const TArray<uint8>& ByteArray);
 	
 	// Convert float to byte array | Convert byte array to float
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websocket||Binary")
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websockets||Binary")
 	static TArray<uint8> FloatToByteArray(const float& value);
 
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websocket||Binary")
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websockets||Binary")
 	static float ByteArrayToFloat(const TArray<uint8>& ByteArray);
 
 	// Convert float to byte array | Convert byte array to float
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websocket||Binary")
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websockets||Binary")
 	static TArray<uint8> FVectorToByteArray(const FVector& value);
 
-	UFUNCTION(BlueprintCallable, Category="Websocket||Binary")
+	UFUNCTION(BlueprintCallable, Category="Websockets||Binary")
 	static FVector ByteArrayToFVector(const TArray<uint8>& ByteArray);
 
 	// Convert float to byte array | Convert byte array to float
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websocket||Binary")
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websockets||Binary")
 	static TArray<uint8> FRotatorToByteArray(const FRotator& value);
 
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websocket||Binary")
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Websockets||Binary")
 	static FRotator ByteArrayToFRotator(const TArray<uint8>& ByteArray);
 };
