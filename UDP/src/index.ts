cdeae6176c2d688032690869fe7d4c5476a11a0b
Nathan Miguel <nathan.cm77@gmail.com>
2024-06-30 18:11:20 -0300

    Minor improvements while send messages
     On branch nodejs
     Your branch is up to date with 'origin/nodejs'.
    
     Changes to be committed:
    	modified:   UDP/src/index.ts

--- UDP/src/index.ts
+++ UDP/src/index.ts
@@ -1,16 +1,19 @@
-import { Address } from 'cluster';
-import * as dgram from 'dgram';
-import { AddressInfo } from 'net';
+import { Address } from "cluster";
+import * as dgram from "dgram";
+import { AddressInfo } from "net";
 
-const HOST: string = 'localhost';
+const HOST: string = "localhost";
 const PORT: number = 3000;
 
-const server: dgram.Socket = dgram.createSocket('udp4');
+const server: dgram.Socket = dgram.createSocket("udp4");
 
-server.on('message', (msg: Buffer, rinfo: dgram.RemoteInfo) => {
-  console.log(`Client: ${msg} de ${rinfo.address}:${rinfo.port}`);
+server.on("message", (msg: Buffer, rinfo: dgram.RemoteInfo) => {
+  console.log(`Client: ${rinfo.address}:${rinfo.port}`);
+  console.log(`Message: ${msg}`);
 
-  const response: string = `Server: ${rinfo.address}:${rinfo.port}`;
+  const response: string = `Server: ${rinfo.address}:${
+    rinfo.port
+  } \nMessage: ${msg.toString()}`;
   server.send(response, rinfo.port, rinfo.address, (error) => {
     if (error) {
       console.error(`Erro trying send message: ${error.message}`);
@@ -20,12 +23,12 @@ server.on('message', (msg: Buffer, rinfo: dgram.RemoteInfo) => {
   });
 });
 
-server.on('listening', () => {
+server.on("listening", () => {
   const address: AddressInfo = server.address();
   console.log(`Listening at adress: ${address.address}:${address.port}`);
 });
 
-server.on('error', (error: Error) => {
+server.on("error", (error: Error) => {
   console.error(`Server error: ${error.message}`);
   server.close();
 });
