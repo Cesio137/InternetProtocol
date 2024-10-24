806f533bb4cd22fa5529b9ba04832d345662c59d
Nathan Miguel <nathan.cm77@gmail.com>
2024-10-24 14:41:13 -0300

    added json examples

--- Source/InternetProtocol/Public/Library/InternetProtocolFunctionLibrary.h
+++ Source/InternetProtocol/Public/Library/InternetProtocolFunctionLibrary.h
@@ -10,7 +10,7 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(Blueprintable, BlueprintType, Category = "IP")
 class INTERNETPROTOCOL_API UInternetProtocolFunctionLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
