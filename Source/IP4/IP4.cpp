042638333a5101dbacd76696772f1f6f2fda4bf9
Nathan Miguel <nathan.cm77@gmail.com>
2024-11-08 13:18:06 -0300

    Updated example base

---
+++ Source/IP4/IP4.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "IP4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IP4, "IP4" );
---
+++ Source/IP4/IP4GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IP4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IP4_API AIP4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
