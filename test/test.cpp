ae2c539b27fe26a916bb04fd8602f0b2c8f6cbe4
Nathan Miguel <nathan.cm77@gmail.com>
2025-02-08 21:55:42 -0300

    Remove unused header management code from test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -2,18 +2,7 @@
 #define ASIO_USE_OPENSSL
 #include "IP/InternetProtocol.hpp"
 
-std::map<std::string, std::string> headers;
-std::map<std::string, std::string> &get_headers() {
-    return headers;
-}
-
 int main() {
-    std::map<std::string, std::string> headers2;
-    headers2.insert_or_assign("Olá", "Mundo");
-    get_headers() = headers2;
-    //get_headers().insert_or_assign("Olá", "Mundo");
-    std::cout << get_headers().at("Olá") << std::endl;
-    std::cout << headers.at("Olá") << std::endl;
 
     return 0;
 }
